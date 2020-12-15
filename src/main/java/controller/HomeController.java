91439aac0beab1121e0873d5e44fa72a95064d45
manki <skxz123@gmail.com>
2020-12-15 22:27:51 +0900

    t

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/mvc")
+@RequestMapping(value = "/")
 public class HomeController {
 
     @GetMapping(value = "/")
