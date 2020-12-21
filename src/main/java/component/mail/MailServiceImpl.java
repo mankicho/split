350b98e0e7fed6fa85fee8de9b35f38c1e888e69
manki <skxz123@gmail.com>
2020-12-21 23:59:12 +0900

    mail

--- src/main/java/component/mail/MailServiceImpl.java
+++ src/main/java/component/mail/MailServiceImpl.java
@@ -3,6 +3,7 @@ package component.mail;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
