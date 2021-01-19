f078571e278bdd7a0a773c6a489adbaac7a7f3d7
manki <skxz123@gmail.com>
2021-01-19 22:24:07 +0900

    echo test

--- src/main/java/websocket/EchoHandler.java
+++ src/main/java/websocket/EchoHandler.java
@@ -35,6 +35,7 @@ public class EchoHandler extends TextWebSocketHandler {
 
     // 1대1
     private Map<String, WebSocketSession> userSessionsMap = new HashMap<>();
+    private Map<String, WebSocketSession> sessionIdMap = new HashMap<>();
 
     /**
      * @param session this function called when client' session is connected
@@ -42,10 +43,11 @@ public class EchoHandler extends TextWebSocketHandler {
      */
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println(session.getId()+" is connect");
+        System.out.println(session.getId() + " is connect");
         sessions.add(session);
         String senderEmail = getEmail(session);
         userSessionsMap.put(senderEmail, session);
+        sessionIdMap.put(session.getId(), session);
     }
 
     /**
@@ -81,12 +83,13 @@ public class EchoHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         // todo 1. 적절한 요청인가? (email 이 실제로 DB 에 저장되어있는가)
         // todo 2. 메세지 요청 유형 파악
-        AlarmMessage alarmMessage = alarmMessageParser.parse(message);
-        // todo 3. 유형에 맞는 메소드 수행
-        AlarmMessageServiceExecutor serviceExecutor = new AlarmMessageServiceExecutor(alarmMessage, session, sessions, userSessionsMap);
-        serviceExecutor.execute();
+//        AlarmMessage alarmMessage = alarmMessageParser.parse(message);
+//        // todo 3. 유형에 맞는 메소드 수행
+//        AlarmMessageServiceExecutor serviceExecutor = new AlarmMessageServiceExecutor(alarmMessage, session, sessions, userSessionsMap);
+//        serviceExecutor.execute();
+        TextMessage msg = new TextMessage(session.getId());
+        session.sendMessage(msg);
     }
-
 
     /**
      * @param session client -> server (contains header("user",{user_email})
