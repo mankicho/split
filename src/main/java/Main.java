3ae8e07282efbde3f0b82fe5d011b44351aa70d6
manki <skxz123@gmail.com>
2021-02-10 14:50:33 +0900

    pom.xml - file_upload library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("start");
+        System.out.println(UUID.randomUUID().toString());
         System.out.println(LocalDateTime.now());
     }
 
--- src/main/java/controller/FileUploadController.java
+++ src/main/java/controller/FileUploadController.java
@@ -1,18 +1,32 @@
 package controller;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping(value = "/peed")
 public class FileUploadController {
 
     @PostMapping(value = "/file/upload")
-    public void fileUpload(HttpServletRequest req) {
-        String path = req.getSession().getServletContext().getRealPath("/resources"); // 파일 경로
+    public void fileUpload(HttpServletRequest req, @RequestParam("file") MultipartFile multipartFile) {
+        String path = req.getSession().getServletContext().getRealPath("/resources")+"/profile"; // 파일 경로
+        File targetFile = new File(path + multipartFile.getOriginalFilename());
+        try {
+            InputStream fileStream = multipartFile.getInputStream();
+            FileUtils.copyInputStreamToFile(fileStream, targetFile);
+        } catch (IOException e) {
+            FileUtils.deleteQuietly(targetFile);
+            e.printStackTrace();
+        }
         System.out.println("path = " + path);
     }
 }
