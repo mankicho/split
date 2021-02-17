da705d2e2deb328fd5cbca2ce07c09c1e987c473
manki <skxz123@gmail.com>
2021-02-17 15:31:43 +0900

    fcm service

---
+++ src/main/java/fcm/FcmNotifier.java
@@ -0,0 +1,12 @@
+package fcm;
+
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+
+@Component
+public class FcmNotifier {
+    public void sendFCM(String tokenId, String title, String content){
+//        FileInputStream refreshToken = new FileInputStream();
+    }
+}
