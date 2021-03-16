ffbf278dc6bfc79f9669569192bbaa56dc6df606
manki <skxz123@gmail.com>
2021-03-16 16:28:14 +0900

    school vo, school mapper

--- src/main/java/component/school/vo/SchoolVO.java
+++ src/main/java/component/school/vo/SchoolVO.java
@@ -6,14 +6,15 @@ import java.util.List;
 
 @Data
 public class SchoolVO {
-    private int categoryId;
-    private int schoolId;
-    private String schoolName;
-    private String setLocation;
-    private String oneLineMessage;
-    private String imagePath;
-    private int allMembers;
-    private int sumPerson;
-    private double avgPerson;
-    private List<SchoolHashTags> hashTags;
+    private int categoryId; // 카테고리 Id
+    private int schoolId; // 학교 Id
+    private String schoolName; // 학교 이름
+    private String setLocation; // 출석 장소
+    private String oneLineMessage; // 한줄메세지
+    private String imagePath; // 학교 대표 사진
+    private int allMembers; // 총 재학생
+    private int sumPerson; // 누적 출석인원
+    private double avgPerson; // 하루 평균 출석인원
+    private int cumulativeAmount; // 누적 출석금액
+    private List<SchoolHashTags> hashTags; // 해시태그
 }
