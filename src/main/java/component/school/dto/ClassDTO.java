447a836e966fa4d9faf6c1e590e7cd61bb52cc18
manki <skxz123@gmail.com>
2021-03-18 00:18:21 +0900

    modify, delete validation

--- src/main/java/component/school/dto/ClassDTO.java
+++ src/main/java/component/school/dto/ClassDTO.java
@@ -2,18 +2,9 @@ package component.school.dto;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
 @Data
 public class ClassDTO {
-    @NotNull
     private int schoolId;
-
-    @NotNull
     private int weekday;
-
-    @NotBlank
     private String selectedDate;
 }
